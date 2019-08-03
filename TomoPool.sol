7bb086083f621e89119603ba44c81e329e3389b4
Do Minh Khai <khaihkd@gmail.com>
2019-08-03 14:50:42 +0700

    fix function missing `public`

--- TomoPool.sol
+++ TomoPool.sol
@@ -9,7 +9,6 @@ interface ICandidateManager {
 
 contract CandidateManager is Ownable, ICandidateManager {
     using SafeMath for uint256;
-    // TODO: will update = 900 block
     uint256 constant public BLOCK_PER_EPOCH = 900;
 
     constructor () public {
@@ -473,7 +472,7 @@ contract CandidateContract {
         return governance.supportCap >= capacity.mul(66).div(100);
     }
 
-    function resignIfUnder60k() onlyStaker {
+    function resignIfUnder60k() public onlyStaker {
         uint256 _currentEpoch = currentEpoch();
         require(capacity < 60000 * 10**18 && lastEpochCapUnder60k > 0 && _currentEpoch >= NUM_EPOCH_UNDER60k_TO_RESIGN.add(lastEpochCapUnder60k), "Forced resign is only allowed if the node has 60k for 10 consecutive days");
         resignInternal();
